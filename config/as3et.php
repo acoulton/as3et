845e5018c8a338a9187c87539e8f63abd6d2fd64
Andrew Coulton <andrewnfcoulton@gmail.com>
2012-04-17 11:30:51 +0100

    Document config options

--- config/as3et.php
+++ config/as3et.php
@@ -3,6 +3,9 @@
  * Default As3et configuration file - override at application level as required.
  */
 return array(
+	/*
+	 * One of As3et::MODE_LOCAL or As3et::MODE_S3 - determines where to serve from
+	 */
 	'mode' => NULL,
 	's3' => array(
 		/*
@@ -37,10 +40,32 @@ return array(
 		 */
 		'certificate_authority' => FALSE,
 	),
+
+	/*
+	 * Array of file paths to blacklist and never upload to S3 - supports wildcards:
+	 *
+	 *     'less/*' => TRUE,
+	 *     'Thumbs.db' => TRUE
+	 */
 	'blacklist' => array(
 	),
+
+	/*
+	 * Headers to have Amazon send back with the assets when they're served -
+	 * you could add a far-future expires header here as the path will always
+	 * change when a new version is deployed.
+	 */
 	'asset_headers' => array(
 	),
+
+	/*
+	 * MIME type to set for an asset when the extension is unrecognised
+	 */
 	'default_mime_type' => 'application/octet-stream',
+
+	/*
+	 * The path on disk where the current revision_file will be saved - this is
+	 * the PHP file that returns the SHA of the last deployed revision.
+	 */
 	'revision_file' => APPPATH.'config/as3et_current_revision.php',
 );
